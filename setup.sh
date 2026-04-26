02ba88f15dc5e52e9bbb39a69ec4bbb6156c9fb2
tungtx57-pixel <tungtx57@gmail.com>
2026-04-27 01:11:20 +0700

    setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ TEMP_DIR="/tmp/opencode_ksu_$$"
 echo "Installing KSU Skills for OpenCode..."
 
 rm -rf "${TEMP_DIR}"
-git clone https://github.com/DinhQuangDoi/opencode_skill_ksu.git "${TEMP_DIR}"
+git clone https://github.com/tungtx57-pixel/opencode_skill_ksu.git "${TEMP_DIR}"
 
 mkdir -p "${SKILLS_DIR}"
 cp -r "${TEMP_DIR}/skills/"* "${SKILLS_DIR}/"
